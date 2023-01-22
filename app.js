f6b9a1fcc7e48df4517192f3fe33c8cad9a51545
skipprrr <73844164+skipprrr@users.noreply.github.com>
2023-01-22 21:36:13 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+let button = document.getElementById("my-button");
+
+button.addEventListener("mouseenter", function() {
+  button.style.position = "absolute";
+  button.style.top = Math.random() * 300 + "px";
+  button.style.left = Math.random() * 300 + "px";
+});
+const buttons = document.getElementById("myButton");
+buttons.addEventListener("click", function() {
+  alert("yayyyyyyy i loooooove u<3");
+});
+
+
+  
